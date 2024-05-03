20726303b93a93bd359a6cd8e39779c75881d347
lupus420 <skromny150@gmial.com>
2024-05-03 14:04:52 +0200

    make superuser site

--- SongApp/admin.py
+++ SongApp/admin.py
@@ -1,6 +1,32 @@
 from django.contrib import admin
 
-from .models import Song, Playlist
+from .models import Song, Playlist, PlaylistSong
 
-admin.site.register(Song)
-admin.site.register(Playlist)
+class PlaylistSongInline(admin.TabularInline):
+  model = PlaylistSong
+  extra = 1
+  fk_name = 'playlist'
+  verbose_name = 'Song'
+  verbose_name_plural = 'Songs'
+  fields = ['song']
+  show_change_link = True
+  extra = 0
+
+class PlaylistInline(admin.TabularInline):
+  model = PlaylistSong
+  extra = 1
+  fk_name = 'song'
+  verbose_name = 'Playlist'
+  verbose_name_plural = 'Playlists'
+  fields = ['playlist']
+  show_change_link = True
+  extra = 0
+
+class SongAdmin(admin.ModelAdmin):
+  inlines = [PlaylistInline]
+
+class PlaylistAdmin(admin.ModelAdmin):
+  inlines = [PlaylistSongInline]
+
+admin.site.register(Song, SongAdmin)
+admin.site.register(Playlist, PlaylistAdmin)
